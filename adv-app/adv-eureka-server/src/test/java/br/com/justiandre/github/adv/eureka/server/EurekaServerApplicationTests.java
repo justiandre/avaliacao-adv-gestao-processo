78f87a26eb93d2d8c0b2a634103cd8f430bee5f3
André Justi <justi.andre@gmail.com>
2017-07-06 23:26:58 -0300

    temp

--- adv-app/adv-eureka-server/src/test/java/br/com/justiandre/github/adv/eureka/server/EurekaServerApplicationTests.java
+++ adv-app/adv-eureka-server/src/test/java/br/com/justiandre/github/adv/eureka/server/EurekaServerApplicationTests.java
@@ -1,16 +1,30 @@
+
 package br.com.justiandre.github.adv.eureka.server;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.cloud.config.client.ConfigClientAutoConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ImportAutoConfiguration(exclude = ConfigClientAutoConfiguration.class)
 public class EurekaServerApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+	private static final String PATH_SERVER = "/";
 
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void verificaStartServidor() {
+		boolean requestExecutadoSucesso = this.restTemplate.getForEntity(EurekaServerApplicationTests.PATH_SERVER, Void.class).getStatusCode().is2xxSuccessful();
+		Assert.assertTrue("Servidor não subiu como esperado", requestExecutadoSucesso);
+	}
 }
